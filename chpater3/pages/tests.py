b72b37b8d3be1ceceebc02987dbb988b057c3ff0
DESKTOP-LA12F0T\Abtin <abtinzand9@gmail.com>
2024-01-26 19:41:06 +0330

    chpter 3 complated

--- chpater3/pages/tests.py
+++ chpater3/pages/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
-
+from django.test import SimpleTestCase
+from django.urls import reverse
 # Create your tests here.
+class HomePageTests(SimpleTestCase):
+    def test_url_exists_correct_lacation(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code , 200)
+    def test_url_avalilable_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code , 200)
+    
+class AboutPageTests(SimpleTestCase):
+    def test_url_exists_correct_lacation(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code , 200)
+    def test_url_avalilable_by_name(self):
+        response = self.client.get(reverse("about"))
+        self.assertEqual(response.status_code , 200)
