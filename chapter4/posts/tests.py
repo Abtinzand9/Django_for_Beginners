ccd6c0d7049327f09785d1eafcfbb4efcf77d6c7
DESKTOP-LA12F0T\Abtin <abtinzand9@gmail.com>
2024-01-28 17:46:55 +0330

    chapter4 test complate

--- chapter4/posts/tests.py
+++ chapter4/posts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+class PostsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text = "this is test")
+    def test_model_contet(self):
+        self.assertEqual(self.post.text,"this is test")
+    def test_url_exists_at_correct_location(self):
+        response= self.client.get("/")
+        self.assertEqual(response.status_code,200)
+    def test_hompage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code ,200)
+        self.assertTemplateUsed(response,"posts/home.html")
+        self.assertContains(response , "this is test")
