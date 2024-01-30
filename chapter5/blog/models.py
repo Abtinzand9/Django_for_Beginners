74bafe721d2b6f996eb7fb9bf0c097e830274254
DESKTOP-LA12F0T\Abtin <abtinzand9@gmail.com>
2024-01-30 12:26:39 +0330

    static files

--- chapter5/blog/models.py
+++ chapter5/blog/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
-class Blog (models.Model):
-    title = models.models.CharField(max_length=50)
+class Post (models.Model):
+    title = models.CharField(max_length=50)
     author = models.ForeignKey(
         'auth.User',
         on_delete =models.CASCADE,
     )
     body = models.TextField()
     def __str__(self):
-        self.title
+        return self.title
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
     
