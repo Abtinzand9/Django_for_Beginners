ed34733ceb099c13ae977395d9f6d51f93ca9f86
DESKTOP-LA12F0T\Abtin <abtinzand9@gmail.com>
2024-02-13 17:43:03 +0330

    chapter 8

---
+++ chapter8/accounts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import CustomUser 
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm , CustomUserCreationForm
+# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = [
+        "email",
+        'username',
+        'age',
+        'is_staff',
+    ]
+    fieldsets = UserAdmin.fieldsets +  ((None , {'fields': ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None , {'fields': ('age',)}),)
+
+admin.site.register(CustomUser , CustomUserAdmin)
